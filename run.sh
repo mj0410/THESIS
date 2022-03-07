5e9fb21d7a59a710c3fb6ae29645baa636bede5e
mj0410 <66175878+mj0410@users.noreply.github.com>
2022-03-07 13:37:58 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,24 +14,21 @@ export DTOUTPUT='synthea100_csv_test_rcol_test'
 cd $ORIGIN/table_orientation
 python DeepTablePred.py -m $DT_MODEL_DIR/model_100.hdf5 -i $INPUT_DIR -o $DTOUTPUT
 
-#cd $BASEPATH/model
-#python pred_CRF.py -dt $DTOUTPUT -n 50 -m CRF_rcol_t18_50-fold -o t18_rcol_csv_test_newscripts -t $LDANAME
+while true
+do
+  echo "Continue to semantic type detection YES[y] NO[n]"
+  read ans
 
-#while true
-#do
-#  echo "Continue to semantic type detection YES[y] NO[n]"
-#  read ans
-
-#  if [[ "$ans" == "y" ]] || [[ "$ans" == "yes" ]]; then
-#    cd $BASEPATH/model
-#    python pred_CRF.py -dt $DTOUTPUT -n 50 -m CRF_rcol_t18_50-fold -o t18_rcol_csv_test_newscripts -t $LDANAME
-#    break
-#  elif [[ "$ans" == "n" ]] || [[ "$ans" == "no" ]]; then
-#    echo "quit"
-#    break
-#  else
-#    echo "please enter yes/y or no/n"
-#  fi
-#done
+  if [[ "$ans" == "y" ]] || [[ "$ans" == "yes" ]]; then
+    cd $BASEPATH/model
+    python pred_CRF.py -dt $DTOUTPUT -n 50 -m CRF_rcol_t18_50-fold -o t18_rcol_csv_test_newscripts -t $LDANAME
+    break
+  elif [[ "$ans" == "n" ]] || [[ "$ans" == "no" ]]; then
+    echo "quit"
+    break
+  else
+    echo "please enter yes/y or no/n"
+  fi
+done
 
 
